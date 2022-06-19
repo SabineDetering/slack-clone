22e4a8b5ffe5bc5532d929c704a747a6b5c5310c
Vilia <vzeisig@gmail.com>
2022-06-19 11:13:02 +0200

    styling mainComp: bottomRow

--- src/app/inputbox/inputbox.component.ts
+++ src/app/inputbox/inputbox.component.ts
@@ -4,6 +4,7 @@ import { Channel } from 'src/models/channel.class';
 import { CurrentChannel } from 'src/models/current-channel.class';
 import { Message } from 'src/models/message.class';
 import { Thread } from 'src/models/thread.class';
+import { AuthService } from 'src/services/auth.service';
 import { DataService } from 'src/services/data.service';
 
 @Component({
@@ -15,39 +16,40 @@ export class InputboxComponent implements OnInit {
 
   private newMessage = new Message();
   private newThread = new Thread();
-
-  // private currentChannel: Channel;
-  private currentChannel: CurrentChannel;
+  public currentChannel: CurrentChannel;
   private currentThread: Thread;
   public userInput: string = ''; // ngModel Input
   @Input('currentMessageId') currentMessageId!: string;
   @Input('messageType') messageType!: string;
 
-  constructor(private Data: DataService) {
-    this.Data.currentThread$.subscribe(thread => this.currentThread = thread)
+  constructor(
+    private Data: DataService, 
+    public Auth: AuthService) {
+    this.Data.currentThread$.subscribe(thread => this.currentThread = thread);
   }
 
   ngOnInit(): void {}
 
   async saveUserInput() {
+    // consider remove this line --> not needed?
     this.currentChannel = await this.Data.currentChannel$.getValue();
 
     if (this.userInput.length > 0) {
       if(this.currentMessageId){
         this.saveEditedMessage();
-      }else {
-        this.addNewMessage()
+      } else {
+        this.addNewMessage();
       }
     } else {
       alert('no input');
     }
   }
-  // TODO --> user and image saving to storage
+
 
   addNewMessage(){
     console.log(this.messageType)
-    if(this.messageType == 'answerMessage'){
-      this.addMessage(this.currentThread.threadID);
+    if(this.messageType == 'answerMessage'){ 
+      this.addMessageToThread(this.currentThread.threadID);
     } else{
       this.addMessageAndThread();
     }
@@ -55,7 +57,7 @@ export class InputboxComponent implements OnInit {
 
   async addMessageAndThread(){
     const uniqueThreadID = await this.createNewThread();
-    await this.addMessage(uniqueThreadID);
+    await this.addMessageToThread(uniqueThreadID);
     this.setFirstMessageInThread();
   }
 
@@ -72,13 +74,15 @@ export class InputboxComponent implements OnInit {
   }
 
 
-  async addMessage(threadID: string, image?: string){
+  async addMessageToThread(threadID: string, image?: string){
     const currentTime = new Date().getTime();
     let uniqueMessageID = this.currentChannel.id + '-' + currentTime + Math.round((Math.random() * 10000)).toString();
 
     this.newMessage.threadID = threadID;
     this.newMessage.messageID = uniqueMessageID;
-    // this.newMessage.authorID = this.Data.currentUser.userID;  // need to be set when authenitcation is creatd
+    this.newMessage.authorID = this.Auth.currentUserId;
+    console.log('currUserId',this.newMessage.authorID);
+    
     // this.message.images = image;
     this.newMessage.timestamp = currentTime;
     this.newMessage.messageText = this.userInput;
@@ -100,7 +104,7 @@ export class InputboxComponent implements OnInit {
   }
 
 
-  //  WYSWYG TextEditor
+  // TODO:  WYSWYG TextEditor
   makeBold() {
     document.execCommand('bold');
   }
