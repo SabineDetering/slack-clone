1f572c73691f36d5f376b1f54e6ad26ecdd7a213
Sabine Detering <sabine.detering@web.de>
2022-07-04 22:16:43 +0200

    cleansing app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { MainContainerComponent } from './main-container/main-container.component';
-import { ThreadContainerComponent } from './thread-container/thread-container.component';
 import { LoggedInGuard } from 'ngx-auth-firebaseui';
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 import { PrivacyNoticeComponent } from './privacy-notice/privacy-notice.component';
@@ -11,13 +10,6 @@ const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'channel', component: MainContainerComponent, canActivate: [LoggedInGuard],},
-/*   {
-    path: 'channel/:channelID', component: MainContainerComponent, canActivate: [LoggedInGuard],
-    children: [
-      { path: 'thread/:threadID', component: ThreadContainerComponent, outlet: 'thread' },
-    ]
-  },
-  { path: 'direct-channel/:directChannelID', component: MainContainerComponent, canActivate: [LoggedInGuard] }, */
   { path: 'legal-notice', component: LegalNoticeComponent },
   { path: 'privacy-notice', component: PrivacyNoticeComponent },
 
