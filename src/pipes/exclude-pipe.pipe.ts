3857d09da9a44a6b627193ba14f7ba9f4c2e94a3
Sabine Detering <sabine.detering@web.de>
2022-07-10 15:25:10 +0200

    Exclude pipe code cleansing

--- src/pipes/exclude-pipe.pipe.ts
+++ src/pipes/exclude-pipe.pipe.ts
@@ -1,19 +1,9 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({ name: 'exclude' })
+@Pipe({ name: 'countElementsExcluding' })
 export class ExcludePipe implements PipeTransform {
-  /**
-   * sorts a JSON array
-   * @param array - JSON array to be sorted
-   * @param prop - property used for sorting
-   * @param direction - descending sorting order if 'desc', ascending otherwise
-   * @returns - sorted JSON array
-   */
+
   transform(array: string[], value: string): number {
-    if (array.find(item => item == value)) {
       return array.length - array.filter((item) => item == value).length;
-    } else {
-      return array.length;
-    }
   }
 }
